86d87ee7dc1865d46edf681544a94c67a4317a49
Brent Murphy <bmurphy@bcmcs.net>
2020-05-08 09:53:26 +1000

    [packages] python

--- packages/Python/package.cmake
+++ packages/Python/package.cmake
@@ -22,22 +22,25 @@ set(PKG_PATCHES_TARGET "01-crosscompile.patch"
                        "05-android-nl_langinfo.patch"
                        "06-apple.patch")
 
-if(CORE_SYSTEM_NAME STREQUAL android)
+if(APPLE)
+  if(CORE_SYSTEM_NAME STREQUAL darwin_embedded)
+    list(APPEND PKG_PATCHES_TARGET "07-darwin_embedded.patch")        
+  endif()
+  list(APPEND PKG_PATCHES_TARGET "08-apple-link-intl-for-locale.patch")
+elseif(CORE_SYSTEM_NAME STREQUAL android)
   list(APPEND PKG_PATCHES_TARGET "08-android-disable-locale.patch")
 else()
   list(APPEND PKG_PATCHES_TARGET "08-link-intl-for-locale.patch")
 endif()
 
-if(CORE_SYSTEM_NAME STREQUAL darwin_embedded)
-  list(APPEND PKG_PATCHES "07-darwin_embedded.patch")
-endif()
-
 set(PKG_CONFIGURE_OPTS_HOST "--disable-shared"
                             "--disable-framework"
                             "--without-pymalloc"
+                            "--without-doc-strings"
                             "--with-system-ffi"
                             "--with-system-expat")
 
+#checking for aarch64-apple-darwin-pkg-config... /Users/Shared/xbmc-depends/kodi-depends/appletvos/x86_64-apple-darwin/bin/pkg-config
 set(PKG_CONFIGURE_OPTS_TARGET "ac_cv_func_gethostbyname_r=no"
                               "ac_cv_header_sys_un_h=yes"
                               "ac_cv_file__dev_ptmx=no"
@@ -49,14 +52,21 @@ set(PKG_CONFIGURE_OPTS_TARGET "ac_cv_func_gethostbyname_r=no"
                               "--with-system-ffi"
                               "--without-pymalloc"
                               "--enable-ipv6")
+               
+if(CORE_SYSTEM_NAME STREQUAL darwin_embedded)
+  list(APPEND PKG_CONFIGURE_OPTS_TARGET "ac_cv_func_clock_settime=no"
+                                        "ac_cv_func_sigaltstack=no"
+                                        "ac_cv_func_vfork_works=no"
+                                        "ac_cv_func_fork=no")
+endif()
 
-set(PKG_MAKE_OPTS_TARGET PYTHON_FOR_BUILD=${INSTALL_HOST_PREFIX}/bin/python3
-                         HOST_PGEN=${INSTALL_HOST_PREFIX}/bin/pgen
+set(PKG_MAKE_OPTS_TARGET PYTHON_FOR_BUILD=${INSTALL_PREFIX_HOST}/bin/python3
+                         HOST_PGEN=${INSTALL_PREFIX_HOST}/bin/pgen
                          CROSS_COMPILE_TARGET=yes
                          libpython3.7.a)
 
-set(PKG_MAKE_INSTALL_OPTS_TARGET PYTHON_FOR_BUILD=${INSTALL_HOST_PREFIX}/bin/python3
-                                 HOST_PGEN=${INSTALL_HOST_PREFIX}/bin/pgen
+set(PKG_MAKE_INSTALL_OPTS_TARGET PYTHON_FOR_BUILD=${INSTALL_PREFIX_HOST}/bin/python3
+                                 HOST_PGEN=${INSTALL_PREFIX_HOST}/bin/pgen
                                  CROSS_COMPILE_TARGET=yes)
 
 
