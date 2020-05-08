e936bce49f3633a9459f03c51c1f786560feecfd
Brent Murphy <bmurphy@bcmcs.net>
2020-05-08 21:25:07 +1000

    [package] JsonSchemaBuilder
    Uses cmake instead of utils and leverages the changes in https://github.com/xbmc/xbmc/pull/17880

--- packages/JsonSchemaBuilder/package.cmake
+++ packages/JsonSchemaBuilder/package.cmake
@@ -1,6 +1,6 @@
 set(PKG_NAME "JsonSchemaBuilder")
-set(PKG_SOURCE_PATH ${WITH_KODI_SOURCE}/tools/depends/native/${PKG_NAME}/src)
+set(PKG_SOURCE_PATH ${WITH_KODI_SOURCE}/tools/depends/native/${PKG_NAME})
 set(PKG_DEPENDS_HOST "toolchain")
-set(PKG_TOOLCHAIN "autotools")
-set(PKG_AUTORECONF ON)
+set(PKG_TOOLCHAIN "cmake")
 
+#set(PKG_CMAKE_OPTS_HOST "-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_HOST}")
