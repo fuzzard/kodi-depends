bf5920f6bb704c20a32ec0fcd0971d6ebda6840b
Brent Murphy <bmurphy@bcmcs.net>
2020-05-08 21:25:37 +1000

    TEMP!: [package] Texturepacker

--- packages/TexturePacker/package.cmake
+++ packages/TexturePacker/package.cmake
@@ -1,6 +1,6 @@
 set(PKG_NAME "TexturePacker")
-set(PKG_VERSION "1.0")
-set(PKG_SHA256 "33f613ae3ea68aa161d261ca03202839b2e7acbaa0229071bf8b618daa5b3a38")
+set(PKG_VERSION "1.0.0")
+set(PKG_SHA256 "65316fa1bb69473d0f4c8da1f0e2d7b2340ead3a3814e9dbf85393f68f545ab8")
 set(PKG_ARCHIVE "${PKG_NAME}-${PKG_VERSION}.tar.gz")
 set(PKG_DEPENDS_HOST "toolchain"
                        "libpng"
@@ -12,5 +12,8 @@ set(PKG_AUTORECONF ON)
 
 set(PKG_CONFIGURE_OPTS_HOST "KODI_SRC_DIR=${WITH_KODI_SOURCE}"
                             "EXTRA_DEFINES=-L${INSTALL_PREFIX_HOST}/lib"
-                            "--enable-static")
+                            "STATIC_FLAG=-enable-static")
 
+if(APPLE)
+  set(PKG_BUILD_CFLAGS_HOST "-DTARGET_DARWIN")
+endif()
