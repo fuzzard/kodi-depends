d2d7933a7621e8cde360e66d037eef9580ca6776
fuzzard <fuzzard@kodi.tv>
2020-05-14 19:29:48 +1000

    [package] gas-preprocessor fixup

--- packages/gas-preprocessor/package.cmake
+++ packages/gas-preprocessor/package.cmake
@@ -4,4 +4,4 @@ set(PKG_SHA256 "3ee764fb17f4a319409588ba65449c9d0fa201328edd9fe176560fe85a0864cd
 set(PKG_ARCHIVE "${PKG_NAME}-${PKG_VERSION}.tar.gz")
 set(PKG_TOOLCHAIN "custom")
 
-set(PKG_INSTALL_COMMAND_HOST cp -f gas-preprocessor.pl ${INSTALL_PREFIX_HOST}/bin)
+set(PKG_INSTALL_COMMAND_HOST mkdir -p ${INSTALL_PREFIX_HOST}/bin && cp -f gas-preprocessor.pl ${INSTALL_PREFIX_HOST}/bin)
