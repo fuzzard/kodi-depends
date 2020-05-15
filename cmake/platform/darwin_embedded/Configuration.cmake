9a9732d16aef46bafabce979f95b2247799bb4c2
fuzzard <fuzzard@kodi.tv>
2020-05-15 23:31:12 +1000

    target_as darwinembedded config

--- cmake/platform/darwin_embedded/Configuration.cmake
+++ cmake/platform/darwin_embedded/Configuration.cmake
@@ -6,4 +6,11 @@ list(APPEND WITH_TARGET_CFLAGS "-fheinous-gnu-extensions -no-cpp-precomp -ftree-
 list(APPEND WITH_TARGET_CXXFLAGS "-std=c++14 -no-cpp-precomp -stdlib=libc++ -arch ${WITH_CPU} -isysroot ${WITH_SDK_PATH}")
 list(APPEND WITH_TARGET_LDFLAGS "-Wl,-search_paths_first -L${WITH_SDK_PATH}/usr/lib -isysroot ${WITH_SDK_PATH} -stdlib=libc++ -arch ${WITH_CPU}")
 
+# TARGET_CC not populated from Toolchainsetup.cmake yet
+# better way to handle this? Relocate target_as assignment elsewhere?
+if(NOT TARGET_CC)
+  set(CMAKE_FIND_ROOT_PATH ${WITH_TOOLCHAIN})
+  find_program(TARGET_CC NAMES clang)
+endif()
+
 set(TARGET_AS "${INSTALL_PREFIX_HOST}/bin/gas-preprocessor.pl -arch aarch64 -- ${TARGET_CC}")
