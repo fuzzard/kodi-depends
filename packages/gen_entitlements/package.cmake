4c61bd5c81f8bd110d27f381a075c1172c36227e
fuzzard <fuzzard@kodi.tv>
2020-05-16 13:45:29 +1000

    [packages] utilise install rather than mkdir/cp

--- packages/gen_entitlements/package.cmake
+++ packages/gen_entitlements/package.cmake
@@ -4,4 +4,5 @@ set(PKG_SHA256 "6b22ac443531590dc517ae386617ef7f614af60d704591dab0253e56dd786ffd
 set(PKG_ARCHIVE "${PKG_NAME}-${PKG_VERSION}.tar.gz")
 set(PKG_TOOLCHAIN "custom")
 
-set(PKG_INSTALL_COMMAND_HOST mkdir -p ${INSTALL_PREFIX_HOST}/bin && cp -f gen_entitlements.py ${INSTALL_PREFIX_HOST}/bin)
+set(PKG_INSTALL_COMMAND_HOST install -d ${INSTALL_PREFIX_HOST}/bin
+                     COMMAND install gen_entitlements.py ${INSTALL_PREFIX_HOST}/bin)
