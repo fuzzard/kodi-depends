c315458828a42bde23aef88d737bd91b2a7537f4
fuzzard <fuzzard@kodi.tv>
2020-05-20 13:30:11 +1000

    [platform] osx flags

--- cmake/platform/osx/Configuration.cmake
+++ cmake/platform/osx/Configuration.cmake
@@ -1,6 +1,6 @@
 set(CORE_SYSTEM_NAME osx)
 set(MESON_SYSTEM darwin)
 
-list(APPEND WITH_TARGET_CFLAGS "-fheinous-gnu-extensions -no-cpp-precomp")
-list(APPEND WITH_TARGET_LDFLAGS "-Wl,-search_paths_first -stdlib=libc++")
-
+list(APPEND WITH_TARGET_CFLAGS "-fheinous-gnu-extensions -no-cpp-precomp -mmacosx-version-min=10.13 -arch ${WITH_CPU} -isysroot ${WITH_SDK_PATH}")
+list(APPEND WITH_TARGET_CXXFLAGS "-stdlib=libc++ ${WITH_TARGET_CFLAGS}")
+list(APPEND WITH_TARGET_LDFLAGS "-Wl,-search_paths_first -stdlib=libc++ -mmacosx-version-min=10.13 -arch ${WITH_CPU} -isysroot ${WITH_SDK_PATH}")
